d3b0b284572e77a95983abebff51aecbf30fa758
AndreyVoroninA <andreivoronin37@mail.ru>
2020-12-20 01:37:31 +0300

    api + page-users

--- grapes-inc/src/components/Users/Users.jsx
+++ grapes-inc/src/components/Users/Users.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './Users.module.css';
+import Axios, * as axios from 'axios';
 
 const Users = (props) => {
+const [nameUser, setNameUser] = useState("");
+const [position, setPosition] = useState("");
+const [userList, setUserList] = useState([]);
+
+useEffect(()=> {
+   Axios.get('http://localhost:3001/api/get').then((response) => {
+      setUserList(response.data)
+   })
+}, [])
+
+const submitUser = () => {
+   Axios.post('http://localhost:3001/api/insert', 
+   {nameUser: nameUser, pos: position}).then(()=> {
+      alert('successful insert');
+   })
+}
    return(
       <div>
          <div> Подгрузка пользователей... </div>
 
          <div className={style.usersForm}>
             <label>Сотрудник:</label>
-            <input type="text" name="FIO"/>
+            <input type="text" name="FIO" onChange={(e)=> {setNameUser(e.target.value)}}/>
             <label>Должность:</label>
-            <input type="text" name="position"/>
-            <button>Addn</button>
+            <input type="text" name="position" onChange={(e)=> {setPosition(e.target.value)}}/>
+            <button onClick={submitUser}>Add</button>
+
+            {userList.map((value)=>{
+               return (<h1>Имя - {value.nameUser} | Должность - {value.pos}</h1>
+                  )
+            })}
          </div>
       </div>
    )
--- server/index.js
+++ server/index.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const app = express();
+const mysql = require('mysql');
 
-app.get("/", (req, res) => {
-   res.send("hello world!");
+const pool = mysql.createPool({
+   host: "localhost",
+   port: "3306",
+   user: "root",
+   database: "grapes_db",
+   password: "admin"
+ });
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/get', (req,res) => {
+   const sqlSelect = "SELECT * FROM users";
+   pool.query(sqlSelect, (err, result) => {
+     res.send(result);
+   })
 })
+
+app.post('/api/insert', (req, res) => {
+
+   const nameUser = req.body.nameUser
+   const pos = req.body.pos
+
+   const sqlInsert = "INSERT INTO users (nameUser, pos) VALUES (?,?)"
+   pool.query(sqlInsert, [nameUser, pos], (err, result) => {
+      console.log(result);
+   })
+})
+
+
 
 app.listen(3001, () => {
    console.log("running on port 3001");
